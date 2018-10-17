15dcf72e8ab897ab152a499e12ddc84ddaa3df35
Sofia Zaid <sofiazaid@gmail.com>
2018-10-17 10:26:54 +0200

    Added onmodelcreating to dbcontext class.

--- CarRental.Data/CarRentalContext.cs
+++ CarRental.Data/CarRentalContext.cs
@@ -15,5 +15,10 @@ namespace CarRental.Data
         //One booking can have many cars.
         public DbSet<BookingCar> BookingCars { get; set; }
 
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookingCar>().HasKey(x => new { x.BookingId, x.CarId });
+
+        }
     }
 }
