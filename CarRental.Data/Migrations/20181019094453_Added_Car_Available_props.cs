423d747d90a08cd9a7bc101832f7ad4b4662f118
erolss <erik@eolsson.com>
2018-10-19 11:54:14 +0200

    migrationfiles

---
+++ CarRental.Data/Migrations/20181019094453_Added_Car_Available_props.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRental.Data2.Migrations
+{
+    public partial class Added_Car_Available_props : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Available",
+                table: "Cars",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Cars",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Available",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Cars");
+        }
+    }
+}
